0ce9a37f1f396fe219a44eebaac1e7da69c1c22c
Henry Mao <admin@calclavia.com>
2017-01-20 17:48:42 -0800

    Removed variable scopes

--- a3c.py
+++ a3c.py
@@ -14,61 +14,58 @@ from .util import *
 from .memory import Memory
 
 class ACModel:
-    def __init__(self, model_builder, scope, beta):
-        self.scope = scope
+    def __init__(self, model_builder, beta):
         # Entropy weight
         self.beta = beta
 
-        with tf.variable_scope(self.scope):
-            self.model = model_builder()
-            # Output layers for policy and value estimations
-            self.policies = self.model.outputs[:-1]
-            self.value = self.model.outputs[-1]
+        self.model = model_builder()
+        # Output layers for policy and value estimations
+        self.policies = self.model.outputs[:-1]
+        self.value = self.model.outputs[-1]
 
     def compile(self, optimizer, grad_clip):
         # Only the worker network need ops for loss functions and gradient updating.
-        with tf.variable_scope(self.scope):
-            self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
-            self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
+        self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+        self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
 
-            # Action chosen for every single policy output
-            self.actions = []
-            policy_losses = []
-            entropies = []
+        # Action chosen for every single policy output
+        self.actions = []
+        policy_losses = []
+        entropies = []
 
-            # Every policy output
-            for policy in self.policies:
-                num_actions = policy.get_shape()[1]
-                action = tf.placeholder(shape=[None], dtype=tf.int32)
-                actions_hot = tf.one_hot(action, num_actions)
-                self.actions.append(action)
+        # Every policy output
+        for policy in self.policies:
+            num_actions = policy.get_shape()[1]
+            action = tf.placeholder(shape=[None], dtype=tf.int32)
+            actions_hot = tf.one_hot(action, num_actions)
+            self.actions.append(action)
 
-                responsible_outputs = tf.reduce_sum(policy * actions_hot, [1])
-                # Entropy regularization
-                # TODO: Clipping should be configurable
-                entropies.append(-tf.reduce_sum(policy * tf.log(tf.clip_by_value(policy, 1e-20, 1.0))))
-                # Policy loss
-                policy_losses.append(-tf.reduce_sum(tf.log(responsible_outputs) * self.advantages))
+            responsible_outputs = tf.reduce_sum(policy * actions_hot, [1])
+            # Entropy regularization
+            # TODO: Clipping should be configurable
+            entropies.append(-tf.reduce_sum(policy * tf.log(tf.clip_by_value(policy, 1e-20, 1.0))))
+            # Policy loss
+            policy_losses.append(-tf.reduce_sum(tf.log(responsible_outputs) * self.advantages))
 
-            # Compute average policy and entropy loss
-            self.policy_loss = tf.reduce_sum(policy_losses)
-            self.entropy = tf.reduce_sum(entropies)
+        # Compute average policy and entropy loss
+        self.policy_loss = tf.reduce_sum(policy_losses)
+        self.entropy = tf.reduce_sum(entropies)
 
-             # Value loss (Mean squared error)
-            self.value_loss = tf.reduce_mean(tf.square(self.target_v - tf.reshape(self.value, [-1])))
-            # Learning rate for Critic is half of Actor's, so multiply by 0.5
-            self.loss = 0.5 * self.value_loss + self.policy_loss - self.beta * self.entropy
+         # Value loss (Mean squared error)
+        self.value_loss = tf.reduce_mean(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+        # Learning rate for Critic is half of Actor's, so multiply by 0.5
+        self.loss = 0.5 * self.value_loss + self.policy_loss - self.beta * self.entropy
 
-            # Get gradients from local network using local losses
-            local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
-            self.gradients = tf.gradients(self.loss, local_vars)
-            self.var_norms = tf.global_norm(local_vars)
-            # Clip norm of gradients
-            grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, grad_clip)
+        # Get gradients from local network using local losses
+        local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+        self.gradients = tf.gradients(self.loss, local_vars)
+        self.var_norms = tf.global_norm(local_vars)
+        # Clip norm of gradients
+        grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, grad_clip)
 
-            # Apply local gradients to global network
-            global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
-            self.train = optimizer.apply_gradients(zip(grads, global_vars))
+        # Apply local gradients to global network
+        global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+        self.train = optimizer.apply_gradients(zip(grads, global_vars))
 
 class A3CAgent:
     def __init__(self,
@@ -86,7 +83,7 @@ class A3CAgent:
         self.batch_size = batch_size
         self.save_count = 0
         # Generate global network
-        self.model = ACModel(model_builder, 'global', entropy_factor)
+        self.model = ACModel(model_builder, entropy_factor)
         self.saver = tf.train.Saver(max_to_keep=5)
 
     def load(self, sess):
